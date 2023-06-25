f8658a49217fc0cc6149b506ace1d82efc175858
JunHyeongKim73 <rla5764v@gmail.com>
2023-06-25 14:16:41 +0900

    feat: initial commit

---
+++ src/main/java/com/example/springbasicprinciple/SpringBasicPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasicprinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicPrincipleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicPrincipleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbasicprinciple/SpringBasicPrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasicprinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicPrincipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
