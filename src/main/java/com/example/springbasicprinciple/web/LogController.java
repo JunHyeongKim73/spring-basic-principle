0e16927fae90d73c40f1e6ec994b794c35939b87
JunHyeongKim73 <rla5764v@gmail.com>
2023-07-02 13:31:34 +0900

    feat: [15] 프록시를 통한 웹 스코프 빈 생성 지연

--- src/main/java/com/example/springbasicprinciple/web/LogController.java
+++ src/main/java/com/example/springbasicprinciple/web/LogController.java
@@ -2,7 +2,6 @@ package com.example.springbasicprinciple.web;
 
 import com.example.springbasicprinciple.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,17 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogController {
 
     private final LogService logService;
-    private final ObjectProvider<MyLogger> provider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        final MyLogger myLogger = provider.getObject();
         final String url = request.getRequestURL().toString();
         myLogger.setRequestURL(url);
 
         myLogger.log("controller test");
-        logService.logic("test ID" );
+        logService.logic("test ID");
 
         return "OK";
     }
--- src/main/java/com/example/springbasicprinciple/web/LogService.java
+++ src/main/java/com/example/springbasicprinciple/web/LogService.java
@@ -2,17 +2,15 @@ package com.example.springbasicprinciple.web;
 
 import com.example.springbasicprinciple.common.MyLogger;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogService {
 
-    private final ObjectProvider<MyLogger> provider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        final MyLogger myLogger = provider.getObject();
         myLogger.log("service id = " + id);
     }
 }
